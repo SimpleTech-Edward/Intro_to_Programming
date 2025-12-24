2873905e0adb8d9b823cb33725b4e39478d1c73a
Edward Im <eddieim@gmail.com>
2025-12-23 21:27:10 -0800

    create files for lessons

---
+++ video_01_variables.py
@@ -0,0 +1,34 @@
+"""
+VARIABLES
+symbolic names that act as labels or containers for storing data in memory
+
+DATA TYPES
+Strings - a sequence of characters, including special characters and numbers
+Integers - whole numbers
+Floats - decimal numbers
+Booleans - a special data type with only two options for values, True or False
+"""
+
+# STRINGS
+name = "Edward Im"
+zip_code = "91210"
+phone_number = "555-555-5555"
+
+# INTEGERS
+age = 35
+count = 10
+
+
+# FLOATS
+item_price = 30.23
+PI = 3.14
+sales_tax = 9.7
+
+# BOOLEANS
+is_cold = True
+is_raining = False
+
+
+# CHECKING FOR DATA TYPES
+print(type("hello"))
+print(isinstance("hello", str))
